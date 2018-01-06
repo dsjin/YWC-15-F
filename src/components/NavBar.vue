15d5ce6de194fae28f3f3b4626d868ab61ebcc67
Thatchakon Jom-ud <57070053@kmitl.ac.th>
2018-01-06 09:24:16 +0700

    push

---
+++ src/components/NavBar.vue
@@ -0,0 +1,13 @@
+<template>
+  <nav class="nav">
+    <a class="nav-link active" href="#">Active</a>
+    <a class="nav-link" href="#">Link</a>
+    <a class="nav-link" href="#">Link</a>
+    <a class="nav-link disabled" href="#">Disabled</a>
+  </nav>
+</template>
+<script>
+export default {
+  name:"Navbar"
+}
+</script>
