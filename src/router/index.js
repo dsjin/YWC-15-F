27c1d3d56345d3a127599962a9c92aa4cbefb10e
young <young@PIM.AC.TH>
2018-01-06 09:35:19 +0700

    add route

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,9 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+
+import Landing from '@/pages/Landing'
+import Campaign from '@/pages/Campaign'
+import Donate from '@/pages/Donate'
 
 Vue.use(Router)
 
@@ -8,8 +11,18 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      name: 'Landing',
+      component: Landing
+    },
+    {
+      path: '/campaign',
+      name: 'Campaign',
+      component: Campaign
+    },
+    {
+      path: '/donate',
+      name: 'Donate',
+      component: Donate
     }
   ]
 })
