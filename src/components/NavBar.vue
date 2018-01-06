30959547dbabea9e4156c4b0c866b527350acac4
young <young@PIM.AC.TH>
2018-01-06 11:09:17 +0700

    add nav item

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -19,6 +19,12 @@
           <a class="nav-link" href="#">Blog</a>
         </li>
 
+        <li class="nav-item">
+            <a class="nav-link" href="#">Contact</a>
+          </li>
+
+          
+
       </ul>
 
     </div>
